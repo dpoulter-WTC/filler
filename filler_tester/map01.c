0b628adf661fb09e606233c9bef4b70f01afd66f
Daniel Poulter <Daniel@Macbook-pro.local>
2018-07-13 13:39:43 +0200

    Added filler tester

---
+++ filler_tester/map01.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map01.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiulian <agiulian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/20 16:24:17 by agiulian          #+#    #+#             */
+/*   Updated: 2018/07/12 22:47:52 by dpoulter         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sfiller.h"
+
+void	map01(t_prm *prm, char player[21])
+{
+	printf("\n\n-------------------------------------------\n");
+	printf("|------------------MAP 1------------------|\n");
+	printf("-------------------------------------------\n\n");
+	if (prm->nb1)
+		map01p1(prm, player);
+	if (prm->nb2)
+		map01p2(prm, player);
+}
+
+void	map01p1(t_prm *prm, char player[21])
+{
+	int		i;
+	int		j;
+	int		score;
+	int		num;
+	double	percentage;
+
+	i = 0;
+	num = 0;
+	while (i < prm->player_nb)
+	{
+		score = 0;
+		j = 0;
+		while (j < prm->nb1)
+		{
+			sprintf(prm->entry, "ruby filler_vm -q -f maps/map01 -p1 \
+			players/%s.filler -p2 players/%s.filler > /dev/null", player, \
+					prm->player_lst[i]);
+			system(prm->entry);
+			bzero(prm->entry, 149);
+			prm->fd = open("filler.trace", O_RDONLY);
+			read(prm->fd, prm->buf, 4096);
+			num++;
+			percentage = (double)(((double)num / (double)prm->nb1) * 100);
+			show_status(percentage);
+			if (strstr(prm->buf, player) && !strstr(prm->buf, "Segfault"))
+				score++;
+			j++;
+		}
+		print_result(player, prm->player_lst[i], prm->nb1, score);
+		i++;
+	}
+}
+
+void	map01p2(t_prm *prm, char player[21])
+{
+	int		i;
+	int		j;
+	int		score;
+	int		num;
+	double	percentage;
+
+	i = 0;
+	num = 0;
+	while (i < prm->player_nb)
+	{
+		score = 0;
+		j = 0;
+		while (j < prm->nb2)
+		{
+			sprintf(prm->entry, "ruby filler_vm -q -f maps/map01 -p1 \
+			players/%s.filler -p2 players/%s.filler > /dev/null", \
+			prm->player_lst[i], player);
+			system(prm->entry);
+			bzero(prm->entry, 149);
+			prm->fd = open("filler.trace", O_RDONLY);
+			read(prm->fd, prm->buf, 4096);
+			num++;
+			percentage = (double)(((double)num / (double)prm->nb1) * 100);
+			show_status(percentage);
+			if (strstr(prm->buf, player) && !strstr(prm->buf, "Segfault"))
+				score++;
+			j++;
+		}
+		print_result(prm->player_lst[i], player, prm->nb2, score);
+		i++;
+	}
+}
